a3e95da92c42d3712f72fe4cf9f1f0c5949fca79
LuisJavierHernandezBautista <luisjavierhdezbta@gmail.com>
2023-07-20 09:57:35 -0600

    edad

--- edad.py
+++ edad.py
@@ -1,3 +1,7 @@
 print("Hola mundo")
 nombre=input("Ingresa tu nombre")
 edad=int(input("Ingresa tu edad"))
+if(edad>=18):
+    print(nombre,"Eres mayor de edad")
+else:
+    print(nombre,"Eres menor de edad")
